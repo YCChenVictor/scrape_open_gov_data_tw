3db156d7d4c48c7fa16bd97e3097b8fd62f57985
Victor <t5204713910@gmail.com>
2020-03-14 21:39:18 +0800

    solve the problem interrupting scraping procedure

--- scrape_data.py
+++ scrape_data.py
@@ -1,26 +1,65 @@
 from functions.scrape_class import ScrapeOneData, MySQLWithPython
-# from functions.f_about_path import parent_path
 import os
-from docs.identity import password, location, database, host, user
+from docs.identity import config, location
+import pandas as pd
+import time
 
-# scrape the title and the file url
-test = ScrapeOneData()
-title, url = test.ScrapeTitleAndDownloadURL(11549)
-test.DownloadCSVFile(title, url, location)
+"""
+5943有資料，可是沒有下載下來
 
-# MySQL command
-print("loading data: ", title, " into", " gov")
-MySQL_EXE = MySQLWithPython(host, user, password, database)
+522 + 65 + 659 + 250 + 529 + 662 + 16 + 1684 + 835 + 1755 + 997 + 833 + 95 + 2651 + 26 + 256 + 94 + 32391
+"""
 
-# execute the MySQL command
+for i in range(5943, 5944):
+    
+    try:
 
-# create table accroding to csv titles
-file_path = location + title + ".csv"
-marks = [" ", ">"]
-title = MySQL_EXE.TableName(title) # No special character in table name
+        print("================================")
+        print("downloading dataset: ", i)    
 
-sqlcommand = MySQL_EXE.CreateTable(file_path, title)
-MySQL_EXE.MysqlCommand(sqlcommand)
+        # scrape the title and the file url with data over 10000
+        test = ScrapeOneData()
+        try:
+            title, url = test.ScrapeTitleAndDownloadURL(i)
+        except KeyboardInterrupt:
+            stored_exception=sys.exc_info()    
 
-# load the csv table into DataBase
-# sqlcommand.LoadTable(path, table_name)
+        # check the number data is above 10000 or not
+        if url is None:
+        	continue
+        if len(pd.read_csv(url).index) < 10000:
+            print("the number of data is too few")
+            continue    
+
+        test.DownloadCSVFile(title, url, location)    
+
+        # the file path
+        file_path = location + title + ".csv"    
+
+        # MySQL command
+        print("loading data: ", title, " into", " gov")
+        MySQL_EXE = MySQLWithPython(config, location)    
+
+        # execute the MySQL command    
+
+        # create table accroding to csv titles
+        print("====================")
+        print("creating table~~~~~")
+        title_for_sql = MySQL_EXE.TableName(title) # No special character in table name
+        sqlcommands = MySQL_EXE.CommandCreateTable(file_path, title_for_sql)
+        MySQL_EXE.ExecuteMysqlCommand(sqlcommands)    
+
+        # load the csv table into DataBase
+        print("==================")
+        print("loading table~~~~~")
+        sqlcommands = MySQL_EXE.CommandLoadTable(file_path, title_for_sql)
+        MySQL_EXE.ExecuteMysqlCommand(sqlcommands)
+
+    except:
+
+        print("Connection refused by the server..")
+        print("Let me sleep for 5 seconds")
+        print("ZZzzzz...")
+        time.sleep(5)
+        print("Was a nice sleep, now let me continue...")
+        continue
