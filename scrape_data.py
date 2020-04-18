ed1d07d64a96fbff563f7b30f553df9279de0d6a
Victor <t5204713910@gmail.com>
2020-04-18 13:52:24 +0800

    just add some note

--- scrape_data.py
+++ scrape_data.py
@@ -8,17 +8,6 @@ import io
 import traceback
 import pickle
 
-"""
-從 5945 開始有資料
-5950 (中華郵政全國營業據點)因為有特殊符號，沒有存進SQL
-5951, 5952 因為.zip 目前無法下載，根本就進不去網頁
-5996 的 SQL error 是因為太多 Empty 在同一個 column，導致給錯 type，這是可以解決的，在一開始決定 type 的時候
-
-'error_during_csv_content': unsupported operand type(s) for <<: 'str' and 'int'(沒有解決)
-因為實在太多 datatype error，唯一的辦法可能就是先全部都設為 TEXT，不過這個方法可以等到第二回scraping的時候
-想辦法放到external hard disk裡
-"""
-
 '''
 dict_data = {}
 with open('./docs/record.pickle', 'wb') as handle:
@@ -27,10 +16,10 @@ with open('./docs/record.pickle', 'wb') as handle:
 
 all_num = 522 + 65 + 659 + 250 + 529 + 662 + 16 + 1684 + 835 + 1755 + 997 + 833 + 95 + 2651 + 26 + 256 + 94 + 32391
 
-for i in range(5996, all_num):
+for i in range(5998, all_num):
 
     print("================================")
-    print("downloading dataset: ", i)    
+    print("downloading dataset: ", i)
 
     data = {}
 
@@ -42,7 +31,7 @@ for i in range(5996, all_num):
         data["url"] = Scrape.csv_url
         print("trying to scrape, ",Scrape.title, "url: ", Scrape.csv_url)
     except KeyboardInterrupt:
-        stored_exception=sys.exc_info()    
+        stored_exception=sys.exc_info()
 
     url = Scrape.csv_url
     if url is None:
